674ed951ea94fefc7ce081860c093f0233a2c475
redwritingcode <redwritingcode@gmail.com>
2018-07-05 13:01:49 +1000

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var myApp = angular.module('myApp',[]);
+myApp.controller('MyController', function MyController($scope){
+    
+$scope.fellow = {
+
+    "id": "5b2a11c6e8ae88891060533c",
+    "isActive": false,
+    "age": 34,
+    "fellowship": "ZESE",
+  
+    "first": "Moreno",
+    "last": "Lyons",
+   
+    "email": "moreno.lyons@zese.net",
+    "about": "Anim culpa duis id qui laboris quis. Fugiat Lorem laboris irure veniam veniam labore nulla elit laboris esse. Eu consectetur fugiat in est fugiat pariatur aliqua ut adipisicing sit non proident officia culpa. Voluptate velit ex id aliquip aliquip fugiat amet labore non consequat. Consequat cillum mollit ut consectetur aliquip sunt velit id et excepteur aute. Irure non irure esse nulla eiusmod adipisicing consequat dolore laboris. Ea excepteur ipsum dolor enim exercitation aute sit tempor laboris ad velit.",
+    "registered": "Monday, June 4, 2018 6:52 PM",
+  }
+})
+
+
